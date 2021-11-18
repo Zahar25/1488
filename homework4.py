a93b3709f730728cd80460bb616e420415a52761
Zahar25 <zpyshny@gmail.com>
2021-11-18 21:01:47 +0200

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,3 +1,8 @@
 n1 = input("Enter first word: ")
 n2 = input("Enter second word: ")
-print (n1 + n2)
+print (n1 + n2)user_number = int(input())
+result = ""
+for b in range (user_number):
+    user_string = (input("Enter string: "))
+    result += result + user_string
+    print (result)
