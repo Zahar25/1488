44355d1fefbb0e874e51b7ca0d10f9fa1bd1b57f
Zahar25 <zpyshny@gmail.com>
2021-11-18 17:39:11 +0200

    Create homework6.py

--- homework6.py
+++ homework6.py
@@ -6,3 +6,14 @@ if i == a:
     print ("True")
 else:
     print ("False")
+def num(n, list):
+    for i in list:
+        if n == i:
+            return True
+        else:
+            return False
+
+
+list = input("Enter list: ").replace(" ", "").split(",")
+n = input("Enter number: ")
+print(num(n, list))
