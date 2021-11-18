f8e28888062296f7ed9a3a9b453909af75f78dce
Zahar Pyshny <56980078+Zahar25@users.noreply.github.com>
2021-11-18 21:14:36 +0200

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -2,5 +2,5 @@ user_number = int(input())
 result = ""
 for b in range (user_number):
     user_string = (input("Enter string: "))
-    result += result + user_string
+    result += user_string
     print (result)
